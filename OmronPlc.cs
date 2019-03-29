a73b0fc8a5be2026904b5d6e8bd727539af7b6fc
idle518 <swiming518@163.com>
2019-03-29 21:40:52 +0800

    欧姆龙plc通信api
    基于omron plc hostlink协议

---
+++ OmronPlc.cs
@@ -0,0 +1,135 @@
+﻿using System.Text;
+using System.IO.Ports;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Idle.Tools.Instrument
+{
+    /// <summary>
+    /// use host link protocal you need to change plc mode to monitor mode.
+    /// </summary>
+    /// 
+    public class OmronPlc:SerialPortInstrument,IPLC
+    {
+        public enum Area{ X=0X30,Y=0x30,WR=0X31,HR=0X32,AR=0X33,DM=0X02,NONE=-1};
+    	public enum MODE{BIT=0,WORD=0x80};
+    	
+        const string _cmdHeader="@00FA00000000001";
+
+        public OmronPlc(string portName)
+            : base(portName, 9600, Parity.Even, 7, StopBits.Two)
+        {
+            _endString = "*\r";
+        }
+        
+        public void SetValue(string device,int state)
+        {
+            Match m = Regex.Match(device.ToUpper(), @"^[XYWHAD][0-9]{1,4}\.{0,1}[0-9]{1,2}");
+            if (m.Length == 0) throw new FormatException("param device format error.");
+            Area area;
+            int startAddr;
+            MODE mode;
+        	DecodeDevice(m.Value,out area,out mode,out startAddr);
+            if ((area<0) || (startAddr < 0)) return;
+        	SetValue(area,mode,startAddr,state);
+        }
+
+        void DecodeDevice(string device, out Area area, out MODE bitOrWord, out int startAddr)
+        {
+            area = (Area)Enum.Parse(typeof(Area), device.Substring(0,1));
+            int index = device.IndexOf('.');
+            int high = 0;
+            int low = 0;
+            if (index > 0)
+            {
+                low = Convert.ToInt32(device.Substring(index + 1));
+                high = Convert.ToInt32(device.Substring(1, index - 1));
+                bitOrWord = MODE.BIT;
+            }
+            else
+            {
+                high = Convert.ToInt32(device.Substring(1,device.Length-1));
+                bitOrWord = MODE.WORD;
+            }
+            startAddr = (high << 8) | low;
+        }
+        
+        public void SetValue(Area area,MODE mode,int startAddr,int state)
+        {
+        	int[] states=new int[]{state};
+        	SetValue(area,mode,startAddr,states);
+        }
+        
+        public void SetValue(Area area,MODE mode,int startAddr,int[] states)
+        {
+        	StringBuilder cmd=new StringBuilder(512);
+        	cmd.Append(_cmdHeader);
+        	cmd.AppendFormat("02{0:X2}{1:X6}{2:X4}",(int)area+(int)mode,startAddr,states.Length);
+        	if(MODE.BIT==mode){
+	        	foreach(int state in states){
+	        		cmd.Append(state==0?"00":"01");
+	        	}
+        	}else{
+        		foreach(int state in states){
+        			cmd.Append(state.ToString("X4"));
+        		}
+        	}
+        	WritePort(cmd);
+        }
+        
+        public int GetValue(string device){
+            Match m = Regex.Match(device.ToUpper(), @"^[XYWHAD][0-9]{1,4}\.{0,1}[0-9]{1,2}");
+            if (m.Length == 0) throw new FormatException("param device format error.") ;
+            Area area;
+            int startAddr;
+            MODE mode;
+            DecodeDevice(m.Value, out area, out mode, out startAddr);
+        	int[] ret = GetValue(area,mode,startAddr,1);
+        	return ret[0];
+        }
+        
+        public int[] GetValue(Area area,MODE mode,int startAddr,int count){
+        	int[] results=new int[count];
+        	int len=2;
+        	
+        	if(MODE.WORD==mode)len=4;
+        	StringBuilder cmd=new StringBuilder(512);
+        	cmd.Append(_cmdHeader);
+        	cmd.AppendFormat("01{0:X2}{1:X6}{2:X4}",(int)area+(int)mode,startAddr,count);
+        	
+        	string ret=WritePort(cmd);
+        	if(null!=ret){
+	        	for(int i=0;i<count;i++){
+	        		results[i]=Convert.ToInt32(ret.Substring(23+i*len,len),16);
+	        	}
+        	}
+        	return results;
+        }
+
+        public int GetValue(Area area, MODE mode, int startAddr)
+        {
+            int[] ret = GetValue(area, mode, startAddr, 1);
+            return ret[0];
+        }
+
+        void CheckFCS(StringBuilder cmd){
+        	int sum=0;
+        	for(int i=0;i<cmd.Length;i++){
+        		sum^=(int)cmd[i];
+        	}
+        	
+        	cmd.AppendFormat("{0:X2}*\r",sum&0xff);
+        }
+
+        string WritePort(StringBuilder cmd)
+        {
+            string ret = null;
+            CheckFCS(cmd);
+            ret = SendAndRecv(cmd.ToString());
+            if ("00" != ret.Substring(21, 2)) { throw new Exception("OMRON plc 通信错误,请检查串口是否配置正确:"+ret); }
+
+            return ret;
+        }
+    }
+}
---
+++ SerialPortInstrument.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace Idle.Tools.Instrument
+{
+    /// <summary>
+    /// 串口通信基类
+    /// </summary>
+    public class SerialPortInstrument:IDisposable
+    {
+        protected string _endString="\r\n";
+        /// <summary>
+        /// 同步锁多线程调用
+        /// </summary>
+        object _objLock = new object();
+        /// <summary>
+        /// 串口
+        /// </summary>
+        SerialPort _port;
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="portName">com名称</param>
+        /// <param name="baudRate">波特率</param>
+        /// <param name="parity">校验</param>
+        /// <param name="dataBits">数据位</param>
+        /// <param name="stopBits">停止位</param>
+        public SerialPortInstrument(string portName,int baudRate,Parity parity,int dataBits,StopBits stopBits)
+        {
+            _port = new SerialPort(portName,baudRate,parity,dataBits,stopBits);
+            _port.ReadTimeout = 5000;
+            _port.WriteTimeout = 2000;
+            _port.Open();
+        }
+
+        public SerialPortInstrument(string portName)
+        {
+            _port = new SerialPort(portName);
+            _port.ReadTimeout = 5000;
+            _port.WriteTimeout = 2000;
+            _port.Open();
+        }
+        /// <summary>
+        /// 释放串口资源
+        /// </summary>
+        public void Dispose()
+        {
+            if (_port != null) _port.Close();
+        }
+        /// <summary>
+        /// 向串口发送消息并等待消息返回
+        /// </summary>
+        /// <param name="msg">发送消息</param>
+        /// <returns>返回的消息</returns>
+        protected string SendAndRecv(string msg)
+        {
+            lock(_objLock)
+            {
+                _port.ReadExisting();
+                _port.Write(msg);
+                return _port.ReadTo(_endString);
+            }
+        }
+        /// <summary>
+        /// 向串口发送消息
+        /// </summary>
+        /// <param name="msg">待发消息</param>
+        protected void SendOnly(string msg)
+        {
+            lock (_objLock)
+            {
+                _port.ReadExisting();
+                _port.Write(msg);
+            }
+        }
+        /// <summary>
+        /// 读取串口消息
+        /// </summary>
+        /// <returns>接收到的消息</returns>
+        protected string RecvOnly()
+        {
+            lock (_objLock)
+            {
+                return _port.ReadTo(_endString);
+            }
+        }
+        /// <summary>
+        /// 设置握手方式
+        /// </summary>
+        protected Handshake Handshake { set { _port.Handshake = value; } }
+        /// <summary>
+        /// 读取串口中剩余数据
+        /// </summary>
+        /// <returns>数据</returns>
+        protected string RecvExisting()
+        {
+            return _port.ReadExisting();
+        }
+    }
+}
